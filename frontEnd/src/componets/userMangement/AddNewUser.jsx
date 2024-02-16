ec9539967d045b9addda4948050077bb76339f9d
jlucas86 <joshua.lucas014@gmail.com>
2024-02-15 23:39:26 -0500

    started componet AddNewUser

---
+++ frontEnd/src/componets/userMangement/AddNewUser.jsx
@@ -0,0 +1,19 @@
+
+function AddNewUser() {
+    return (
+        <div>
+            <h2>Add New User</h2>
+            <form action="">
+                <p>
+                    <input type="text" placeholder="Username" />
+                </p>
+                <p>
+                    <input type="password" name="" id=""  placeholder="Password"/>
+                </p>
+
+            </form>
+        </div>
+    );
+}
+
+export default AddNewUser;
