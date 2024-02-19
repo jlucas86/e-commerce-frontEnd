a6e653772e324a14d5b23e57f50d2032c7af1b25
jlucas86 <joshua.lucas014@gmail.com>
2024-02-18 23:58:34 -0500

    finished setting up tailwind along with adding menu bar

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserManagement from './componets/userManagement/userManagement'
+import MenuBar from './componets/header/menuBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <MenuBar></MenuBar>
       <UserManagement></UserManagement>
     </>
   )
---
+++ frontEnd/src/componets/header/menuBar.jsx
@@ -0,0 +1,11 @@
+function menuBar() {
+    return(
+        <div className="flex border-black border-solid border-2">
+            <button>Menu</button>
+            <h1>Buy Lots</h1>
+            <button> user</button>
+        </div>
+    );
+}
+
+export default menuBar;
