1f7b4c31dc3da52ff97ab738e1476019c08c55e9
jlucas86 <joshua.lucas014@gmail.com>
2024-02-19 23:56:26 -0500

    testing git settings

--- frontEnd/src/componets/header/menuBar.jsx
+++ frontEnd/src/componets/header/menuBar.jsx
@@ -1,9 +1,9 @@
 function menuBar() {
     return(
-        <div className="flex border-black border-solid border-2">
-            <button>Menu</button>
-            <h1>Buy Lots</h1>
-            <button> user</button>
+        <div className="flex flex-col justify-between lg:flex-row w-screen border-black border-solid border-2 rounded">
+            <button >Menu</button>
+            <h1 className="">Buy Lots</h1>
+            <button className=""> user</button>
         </div>
     );
 }
